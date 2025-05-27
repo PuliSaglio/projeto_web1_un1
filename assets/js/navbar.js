494ef3c5921d2734a7b39813dafa0961e4fe0a17
happyholand <josefelipehol@gmail.com>
2025-05-27 20:11:42 -0300

    fix: change active on navbar depending on page

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -22,42 +22,84 @@ document.addEventListener("DOMContentLoaded", () => {
   const navbarContainer = document.getElementById("navbar-container");
   const usuario = JSON.parse(sessionStorage.getItem("usuarioLogado"));
 
-  if (usuario) {
-    // Navbar para usuário logado
-    navbarContainer.innerHTML = `
-      <div class="navbar">
-        <div class="left">
-          <a href="flights.html" class="active">✈️ <span>Voos</span></a>
-          <a href="stays.html">🛏️ <span>Hospedagens</span></a>
-        </div>
-        <div class="logo">
-          <a href="index.html">IMD Viagens</a>
-        </div>
-        <div class="right" id="user-area">
-          <span id="user-nome" class="me-2">Olá, ${usuario.nome.split(" ")[0]}</span>
-          <a href="conta.html" class="btn btn-outline-secondary btn-sm">Minha Conta</a>
-          <button class="btn btn-danger btn-sm ms-2" onclick="logout()">Sair</button>
-        </div>
-      </div>
-    `;
-  } else {
-    // Navbar padrão
-    navbarContainer.innerHTML = `
-      <div class="navbar">
-        <div class="left">
-          <a href="flights.html" class="active">✈️ <span>Voos</span></a>
-          <a href="stays.html">🛏️ <span>Hospedagens</span></a>
-        </div>
-        <div class="logo">
-          <a href="index.html">IMD Viagens</a>
-        </div>
-        <div class="right">
-          <a href="login.html">Login</a>
-          <a href="cadastro.html" class="signup">Cadastro</a>
-        </div>
-      </div>
-    `;
-  }
+  if (navbarContainer.classList.contains("flights")) {
+
+      if (usuario) {
+        // Navbar para usuário logado
+        navbarContainer.innerHTML = `
+          <div class="navbar">
+            <div class="left">
+              <a href="flights.html" class="active">✈️ <span>Voos</span></a>
+              <a href="stays.html">🛏️ <span>Hospedagens</span></a>
+            </div>
+            <div class="logo">
+              <a href="index.html">IMD Viagens</a>
+            </div>
+            <div class="right" id="user-area">
+              <span id="user-nome" class="me-2">Olá, ${usuario.nome.split(" ")[0]}</span>
+              <a href="conta.html" class="btn btn-outline-secondary btn-sm">Minha Conta</a>
+              <button class="btn btn-danger btn-sm ms-2" onclick="logout()">Sair</button>
+            </div>
+          </div>
+        `;
+      } else {
+        // Navbar padrão
+        navbarContainer.innerHTML = `
+          <div class="navbar">
+            <div class="left">
+              <a href="flights.html" id="nav_flights" class="active">✈️ <span>Voos</span></a>
+              <a href="stays.html" id="nav_stays">🛏️ <span>Hospedagens</span></a>
+            </div>
+            <div class="logo">
+              <a href="index.html">IMD Viagens</a>
+            </div>
+            <div class="right">
+              <a href="login.html">Login</a>
+              <a href="cadastro.html" class="signup">Cadastro</a>
+            </div>
+          </div>
+        `;
+      }
+    }
+
+    if (navbarContainer.classList.contains("stays")) {
+      if (usuario) {
+        // Navbar para usuário logado
+        navbarContainer.innerHTML = `
+          <div class="navbar">
+            <div class="left">
+              <a href="flights.html">✈️ <span>Voos</span></a>
+              <a href="stays.html" class="active">🛏️ <span>Hospedagens</span></a>
+            </div>
+            <div class="logo">
+              <a href="index.html">IMD Viagens</a>
+            </div>
+            <div class="right" id="user-area">
+              <span id="user-nome" class="me-2">Olá, ${usuario.nome.split(" ")[0]}</span>
+              <a href="conta.html" class="btn btn-outline-secondary btn-sm">Minha Conta</a>
+              <button class="btn btn-danger btn-sm ms-2" onclick="logout()">Sair</button>
+            </div>
+          </div>
+        `;
+      } else {
+        // Navbar padrão
+        navbarContainer.innerHTML = `
+          <div class="navbar">
+            <div class="left">
+              <a href="flights.html">✈️ <span>Voos</span></a>
+              <a href="stays.html" class="active">🛏️ <span>Hospedagens</span></a>
+            </div>
+            <div class="logo">
+              <a href="index.html">IMD Viagens</a>
+            </div>
+            <div class="right">
+              <a href="login.html">Login</a>
+              <a href="cadastro.html" class="signup">Cadastro</a>
+            </div>
+          </div>
+        `;
+      }
+    }
 });
 
 function logout() {
